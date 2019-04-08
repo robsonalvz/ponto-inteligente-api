75baa2f49a37440bb3d2cd3866a853cf15158bbc
robsonalvz <robson.alves@dce.ufpb.br>
2019-04-08 17:57:26 -0300

    classe funcionario atualizada

--- src/main/java/com/eagle/pontointeligente/api/entities/Empresa.java
+++ src/main/java/com/eagle/pontointeligente/api/entities/Empresa.java
@@ -2,16 +2,22 @@ package com.eagle.pontointeligente.api.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
+
 @Entity
-@Table(name="empresa")
+@Table(name = "empresa")
 public class Empresa implements Serializable {
 
 	/**
@@ -24,13 +30,14 @@ public class Empresa implements Serializable {
 	private String cnpj;
 	private Date dataCriacao;
 	private Date dataAtualizacao;
+	private List<Funcionario> funcionarios;
 
 	public Empresa() {
 
 	}
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Long getId() {
 		return id;
 	}
@@ -78,6 +85,15 @@ public class Empresa implements Serializable {
 	@PreUpdate
 	public void preUpdate() {
 		dataAtualizacao = new Date();
+	}
+
+	@OneToMany(mappedBy= "empresa", fetch= FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
 	}
 
 	@PrePersist
