946389686c6e8a2b62fd7c8990c84c10bb59923c
robsonalvz <robson.alves@dce.ufpb.br>
2019-04-08 17:30:44 -0300

    classe empresa atualizada

--- src/main/java/com/eagle/pontointeligente/api/entities/Empresa.java
+++ src/main/java/com/eagle/pontointeligente/api/entities/Empresa.java
@@ -1,5 +1,93 @@
 package com.eagle.pontointeligente.api.entities;
 
-public class Empresa {
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+
+public class Empresa implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7161950402127366102L;
+
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+
+	public Empresa() {
+
+	}
+
+	@Id
+	@GeneratedValue
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Column(name = "razao_social", nullable = false)
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	@Column(name = "cnpj", nullable = false)
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	@Column(name = "data_criacao", nullable = false)
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	@Column(name = "data_atualizacao", nullable = false)
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	@PreUpdate
+	public void preUpdate() {
+		dataAtualizacao = new Date();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		final Date atual = new Date();
+		dataCriacao = atual;
+		dataAtualizacao = atual;
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+				+ ", dataAtualizacao=" + dataAtualizacao + "]";
+	}
 
 }
